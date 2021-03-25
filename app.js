c2f3b1b55ab4455be1ac136fbb81957e20ffe5a5
xjarvik <sowi18oc@student.ju.se>
2021-03-25 12:27:03 +0100

    Added JSON file generation

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const app = async function(){
         console.log("Error: You must specify a folder.")
         process.exit(0)
     }
+    else if(!args[1]){
+        console.log("Error: You must specify a label.")
+        process.exit(0)
+    }
     else if(args[0].charAt(args[0].length - 1) != "/"){
         args[0] = args[0].concat("/")
     }
@@ -21,18 +25,49 @@ const app = async function(){
     for await(const dirent of dir){
         const fileName = dirent.name
         if(fileName.endsWith(".jpg") || fileName.endsWith(".png")){
+            var obj = {
+                image: fileName,
+                annotations: []
+            }
             const img = fs.readFileSync(args[0] + fileName)
             const predictions = await model.estimateFaces(await pixels(img), false)
             if(predictions.length > 0){
                 annotatedImages++
-                process.stdout.write("Annotated " + annotatedImages + " images...\r")
+                process.stdout.write("\rAnnotated " + annotatedImages + " images...")
             }
             for(let i = 0; i < predictions.length; i++){
                 const start = predictions[i].topLeft
                 const end = predictions[i].bottomRight
+                const size = [end[0] - start[0], end[1] - start[1]]
+                obj.annotations.push({
+                    label: args[1],
+                    coordinates: {
+                        x: start[0] + (size[0] / 2),
+                        y: start[1] + (size[1] / 2),
+                        width: size[0],
+                        height: size[1]
+                    }
+                })
             }
+            array.push(obj)
         }
     }
+
+    console.log("\nCreating JSON file...")
+
+    if(fs.existsSync(args[0] + "annotations.json")){
+        var count = 1
+        do{
+            count++
+        }
+        while(fs.existsSync(args[0] + "annotations" + count + ".json"))
+        fs.writeFileSync(args[0] + "annotations" + count + ".json", JSON.stringify(array))
+    }
+    else{
+        fs.writeFileSync(args[0] + "annotations.json", JSON.stringify(array))
+    }
+
+    console.log("Done.")
 }
 
 app()

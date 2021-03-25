2df8332a26c97d7b41c948da41e852913fa22d43
xjarvik <sowi18oc@student.ju.se>
2021-03-25 10:15:34 +0100

    Added annotating feature using BlazeFace

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
+const tf = require("@tensorflow/tfjs")
 const args = process.argv.slice(2)
 const fs = require("fs")
 const array = []
+const blazeface = require("@tensorflow-models/blazeface")
+const pixels = require("image-pixels")
 
-if(!args[0]){
-    console.log("Error: You must specify a folder.")
-    process.exit(0)
+const app = async function(){
+    const model = await blazeface.load()
+
+    if(!args[0]){
+        console.log("Error: You must specify a folder.")
+        process.exit(0)
+    }
+    else if(args[0].charAt(args[0].length - 1) != "/"){
+        args[0] = args[0].concat("/")
+    }
+    
+    const dir = await fs.promises.opendir(args[0])
+    var annotatedImages = 0
+    for await(const dirent of dir){
+        const fileName = dirent.name
+        if(fileName.endsWith(".jpg") || fileName.endsWith(".png")){
+            const img = fs.readFileSync(args[0] + fileName)
+            const predictions = await model.estimateFaces(await pixels(img), false)
+            if(predictions.length > 0){
+                annotatedImages++
+                process.stdout.write("Annotated " + annotatedImages + " images...\r")
+            }
+            for(let i = 0; i < predictions.length; i++){
+                const start = predictions[i].topLeft
+                const end = predictions[i].bottomRight
+            }
+        }
+    }
 }
 
-fs.readdirSync(args[0]).forEach(function(file){
-    console.log(file)
-})
+app()
